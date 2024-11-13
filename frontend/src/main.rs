08f39fe42f20fbc783ed59c26ec6aee9c2fe34d6
zhaowei <zhaowei@microsoft.com>
2024-11-13 14:40:37 +0800

    ignore frontend/dist

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     sycamore::render(|| {
         view! {
             h1 { "Hello, world!" }
-            p { "This is my first Sycamore app!!!" }
+            p { "This is my first Sycamore app :)" }
         }
     });
 }
